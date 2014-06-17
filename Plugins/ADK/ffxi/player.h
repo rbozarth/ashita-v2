9cda81f304d87142a5ed0cefefe651125a434495
atom0s <atom0s@live.com>
2014-06-17 02:32:03 -0700

    Ashita 2.0.0.16 update for 06/17/2014 update.

--- Plugins/ADK/ffxi/player.h
+++ Plugins/ADK/ffxi/player.h
@@ -178,7 +178,7 @@ namespace Ashita
             unsigned short      LimitPoints;
             unsigned char       MeritPoints;
             unsigned char       LimitMode;
-            unsigned char       Unknown0001[174];
+            unsigned char       Unknown0001[182];
             short               Buffs[32];
         };
     };
